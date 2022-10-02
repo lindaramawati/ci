b9177d4affb59493b4fd643e6217462c839304db
lindaramawati <83651414+lindaramawati@users.noreply.github.com>
2022-10-02 23:09:03 +0700

    perbaruan

---
+++ application/controllers/admin/Mitra.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mitra extends CI_Controller {
+	
+	public function index()
+	{
+		$data['page_title']       = 'Daftar Mitra';
+		$this->load->view('backend/template/meta', $data);
+		$this->load->view('backend/template/navbar');
+		$this->load->view('backend/template/sidebar');
+		$this->load->view('backend/template/header');
+		$this->load->view('backend/mitra/index');
+		$this->load->view('backend/template/footer');
+		$this->load->view('backend/template/js');
+	}
+}
